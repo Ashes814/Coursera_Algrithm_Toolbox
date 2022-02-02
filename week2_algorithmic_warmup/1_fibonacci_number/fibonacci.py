a67262734595622586ed7d1151409737e6948707
Owen <1534661514@qq.com>
2022-02-02 17:22:57 +0800

    Complete week2 test1 Fibonacci

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,30 @@
 # Uses python3
-def calc_fib(n):
+import time
+# def calc_fib1(n):
+#     if (n <= 1):
+#         return n
+#
+#     return calc_fib1(n - 1) + calc_fib1(n - 2)
+
+
+def calc_fib2(n):
     if (n <= 1):
         return n
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    res = [0, 1]
+    for i in range(n-1):
+        res_temp = res[1]
+        res[1] = res[0] + res[1]
+        res[0] = res_temp
+
+    return res[1]
+
 
 n = int(input())
-print(calc_fib(n))
+# t0 = time.time()
+# print(calc_fib1(n))
+# print(time.time() - t0)
+
+t1 = time.time()
+print(calc_fib2(n))
+print(time.time() - t1)
