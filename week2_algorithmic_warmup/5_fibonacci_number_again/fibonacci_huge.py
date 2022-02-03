0cda58d8e9b7bfe0a6deed1786b9848db9dc032e
Owen <1534661514@qq.com>
2022-02-03 16:46:43 +0800

    Pass the problem 5 fibonacci_huge

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -13,7 +13,34 @@ def get_fibonacci_huge_naive(n, m):
 
     return current % m
 
+
+def get_fibonacci_huge_efficient(n, m):
+    if n == 0:
+        return 0
+
+
+    period = 0
+    period_list = []
+
+    previous = 0
+    current  = 1
+
+    for i in range(n-1):
+        period_list.append(previous % m)
+        previous, current = current, previous + current
+        period += 1
+        if previous % m == 0 and current % m == 1:
+            break
+    else:
+        return current % m
+
+    period_list_number = n % period
+
+    return period_list[period_list_number]
+
+
 if __name__ == '__main__':
+    #input = input()
     input = sys.stdin.read();
     n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+    print(get_fibonacci_huge_efficient(n, m))
