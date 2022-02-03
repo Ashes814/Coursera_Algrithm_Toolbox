49fab2e19d88ec945eb14907684ddcac26009cd0
Owen <1534661514@qq.com>
2022-02-03 14:17:02 +0800

    Solve problem 4 gcd

--- week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,5 +1,9 @@
 # Uses python3
 import sys
+def gcd_efficient(a, b):
+
+    if min(a,b):
+
 
 def lcm_naive(a, b):
     for l in range(1, a*b + 1):
@@ -8,8 +12,14 @@ def lcm_naive(a, b):
 
     return a*b
 
+
+
+def lcm_efficient(a, b):
+    pass
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
+    # input = sys.stdin.read()
+    input = input()
     a, b = map(int, input.split())
     print(lcm_naive(a, b))
 
