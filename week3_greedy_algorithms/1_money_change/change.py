794b395560d59cff32437f1de6eb4265abcb3e17
Owen <1534661514@qq.com>
2022-02-07 14:22:09 +0800

    Passing the week3 Problem1 'change'

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -3,8 +3,23 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    n = 0
+    while m > 0:
+        if m >= 10:
+            m = m - 10
+            n += 1
+        elif m >= 5:
+            m = m - 5
+            n += 1
+        elif m >= 1:
+            m = m - 1
+            n += 1
+
+    return n
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
     print(get_change(m))
+
+
+#print(get_change(2), get_change(28), get_change(999))
