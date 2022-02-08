c5b9977838d1ec31c683713771cbe57c9193cd1c
Owen <1534661514@qq.com>
2022-02-08 11:52:40 +0800

    Pass week4 problem4 max dot

--- week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.py
+++ week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.py
@@ -4,9 +4,13 @@ import sys
 
 def max_dot_product(a, b):
     #write your code here
+
+    a_sorted = sorted(a)
+    b_sorted = sorted(b)
+
     res = 0
     for i in range(len(a)):
-        res += a[i] * b[i]
+        res += a_sorted[i] * b_sorted[i]
     return res
 
 if __name__ == '__main__':
@@ -17,3 +21,6 @@ if __name__ == '__main__':
     b = data[(n + 1):]
     print(max_dot_product(a, b))
     
+# print(max_dot_product([23], [39]))
+#
+# print(max_dot_product([1,3,-5], [-2,4,1]))
