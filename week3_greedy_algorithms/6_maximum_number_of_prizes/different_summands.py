02b0caadaf4d27585fab82f5b547a4d79521d109
Owen <1534661514@qq.com>
2022-02-09 10:18:21 +0800

    Modify the method of for loop

--- week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -3,16 +3,16 @@ import sys
 
 def optimal_summands(n):
     summands = []
-    choices = [number for number in range(1, n+1)]
-    current = 0
+    #choices = [number for number in range(1, n+1)]
+    #current = 0
     #write your code here
-    while n > 0:
-        n = n - choices[current]
-        if n > 0 and n > choices[current]:
-            summands.append(choices[current])
-            current += 1
+    for choice in range(1, n+1):
+        n = n - choice
+        if n > 0 and n > choice:
+            summands.append(choice)
+            # current += 1
         else:
-            summands.append(n + choices[current])
+            summands.append(n + choice)
             break
     return summands
 
@@ -26,4 +26,4 @@ if __name__ == '__main__':
 # print(optimal_summands(2))
 # print(optimal_summands(6))
 # print(optimal_summands(8))
-# print(optimal_summands(22))
+# # print(optimal_summands(999999999))
