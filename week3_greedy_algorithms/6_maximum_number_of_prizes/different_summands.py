35401bda5287a36c7e694f39c478c69dfc3996a6
Owen <1534661514@qq.com>
2022-02-09 10:05:16 +0800

    Test the problem6 first time

--- week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -3,7 +3,17 @@ import sys
 
 def optimal_summands(n):
     summands = []
+    choices = [number for number in range(1, n+1)]
+    current = 0
     #write your code here
+    while n > 0:
+        n = n - choices[current]
+        if n > 0 and n > choices[current]:
+            summands.append(choices[current])
+            current += 1
+        else:
+            summands.append(n + choices[current])
+            break
     return summands
 
 if __name__ == '__main__':
@@ -13,3 +23,7 @@ if __name__ == '__main__':
     print(len(summands))
     for x in summands:
         print(x, end=' ')
+# print(optimal_summands(2))
+# print(optimal_summands(6))
+# print(optimal_summands(8))
+# print(optimal_summands(22))
