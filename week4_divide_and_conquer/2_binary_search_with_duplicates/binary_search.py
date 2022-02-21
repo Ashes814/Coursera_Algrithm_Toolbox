e8f371bc8b44dc1a927461d73fa31a8b74b03270
Owen <1534661514@qq.com>
2022-02-21 09:48:49 +0800

    Solve the problem 2

--- week4_divide_and_conquer/2_binary_search_with_duplicates/binary_search.py
+++ week4_divide_and_conquer/2_binary_search_with_duplicates/binary_search.py
@@ -1,6 +1,25 @@
 def binary_search(keys, query):
     # write your code here
-    pass
+    low = 0
+    high = len(keys) - 1
+
+    while low <= high:
+        mid = int(low + (high - low) / 2)
+
+        if query == keys[mid]:
+            if mid - 1 < 0:
+                return mid
+            elif query > keys[mid - 1]:
+                return mid
+            else:
+                high = mid - 1
+
+        elif query < keys[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return -1
 
 
 if __name__ == '__main__':
@@ -14,3 +33,9 @@ if __name__ == '__main__':
 
     for q in input_queries:
         print(binary_search(input_keys, q), end=' ')
+
+
+# for q in [9, 4, 5, 2]:
+#
+#
+#     print(binary_search([2,4,4,4,7,7,9], q), end=" | ")
