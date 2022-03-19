dd9cf5f674cfc4cea0e840c41adca333b7d387d4
Owen <1534661514@qq.com>
2022-03-19 13:30:46 +0800

    week1: upload test file

--- Data_structure/week1_basic_data_structures/1_brackets_in_code/test_homework1.py
+++ Data_structure/week1_basic_data_structures/1_brackets_in_code/test_homework1.py
@@ -1,29 +1,29 @@
 import os
-import unittest
+from test_exe import f
 
+test_path = 'tests/'
+for root, dirs, files in os.walk(test_path):
+    test_files = []
+    answer_files = []
+    for file in files:
+        if file[-2:] != '.a':
+            test_files.append(file)
+        else:
+            answer_files.append(file)
 
-class HomeworkTest(unittest.TestCase):
-    test_path = 'tests/'
-    for root, dirs, files in os.walk(test_path):
-        test_files = []
-        answer_files = []
-        for file in files:
-            if file[-2:] != '.a':
-                test_files.append(file)
-            else:
-                answer_files.append(file)
+for file in test_files:
+    with open(test_path + file, 'r') as f1:
+        input_test = f1.read()
+    with open(test_path + file + '.a', 'r') as f2:
+        answer = f2.read()
+    my_answer = f(input_test)
+    try:
+        assert my_answer == answer
+    except:
+        print("My answer is {}, Correct answer is {}, the number of test {}, the input of test {}".format(
+              my_answer, answer, file, input_test)
+        )
 
-    for file in test_files:
-        with open(test_path + file, 'r') as f1:
-            input_test = f1.read()
-        with open(test_path + file + '.a', 'r') as f2:
-            answer = f2.read()
-
-        def test_function(self, input_test, answer):
-            self.input_test = input_test
-            self.answer = answer
-            my_answer = answer
-            assert self.answer == answer
 
 
 
