a74b822554c21390956cc05f6e5bd8dce47ad94f
Owen <1534661514@qq.com>
2022-03-20 18:12:35 +0800

    week1: problem2

--- Data_structure/week1_basic_data_structures/2_tree_height/tree_height.py
+++ Data_structure/week1_basic_data_structures/2_tree_height/tree_height.py
@@ -3,18 +3,37 @@
 import sys
 import threading
 
+def height(tree, root):
+    if tree[root] == []:
+        return 0
+    max_height = 0
+    for r in range(len(tree[root])):
+        update_height = height(tree, tree[root][r])
+        if update_height > max_height:
+            max_height = update_height
 
+
+    return max_height + 1
 def compute_height(n, parents):
     # Replace this code with a faster implementation
-    max_height = 0
-    for vertex in range(n):
-        height = 0
-        current = vertex
-        while current != -1:
-            height += 1
-            current = parents[current]
-        max_height = max(max_height, height)
-    return max_height
+    # max_height = 0
+    # for vertex in range(n):
+    #     height = 0
+    #     current = vertex
+    #     while current != -1:
+    #         height += 1
+    #         current = parents[current]
+    #     max_height = max(max_height, height)
+    nodes = [[] for i in range(n)]
+    for child_index in range(n):
+        parent_index = parents[child_index]
+        if parent_index == -1:
+            root = child_index
+        else:
+            nodes[parent_index].append(child_index)
+    max_height = height(nodes,root)
+
+    return max_height + 1
 
 
 def main():
