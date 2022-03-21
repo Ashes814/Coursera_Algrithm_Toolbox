c8a51e6eba5de0158c86e95cd5e06a2a4e0b18b0
Owen <1534661514@qq.com>
2022-03-21 13:51:49 +0800

    week1 problem3

--- Data_structure/week1_basic_data_structures/3_network_simulation/process_packages.py
+++ Data_structure/week1_basic_data_structures/3_network_simulation/process_packages.py
@@ -1,4 +1,5 @@
 # python3
+import os
 
 from collections import namedtuple
 
@@ -13,7 +14,18 @@ class Buffer:
 
     def process(self, request):
         # write your code here
-        return Response(False, -1)
+        if self.finish_time == []:
+            self.finish_time.append(request[0] + request[1])
+            return Response(True, request[0])
+        for finish in self.finish_time:
+            if request[0] >= finish:
+                self.finish_time.pop(0)
+        self.finish_time.append(request[0] + request[1])
+        if self.size < len(self.finish_time):
+            return Response(False, -1)
+        else:
+
+            return Response(True, request[-2])
 
 
 def process_requests(requests, buffer):
@@ -24,17 +36,42 @@ def process_requests(requests, buffer):
 
 
 def main():
-    buffer_size, n_requests = map(int, input().split())
-    requests = []
-    for _ in range(n_requests):
-        arrived_at, time_to_process = map(int, input().split())
-        requests.append(Request(arrived_at, time_to_process))
 
-    buffer = Buffer(buffer_size)
-    responses = process_requests(requests, buffer)
 
-    for response in responses:
-        print(response.started_at if not response.was_dropped else -1)
+    test_path = 'tests/'
+    for root, dirs, files in os.walk(test_path):
+        test_files = []
+        answer_files = []
+        for file in files:
+            if file[-2:] != '.a':
+                test_files.append(file)
+            else:
+                answer_files.append(file)
+    for file in test_files[4:5]:
+        print(file)
+        with open(test_path + file, 'r') as f1:
+            input_test = f1.read().strip()
+            line1 = input_test.split('\n')[0]
+            buffer_size, n_requests = int(line1.split(' ')[0]), int(line1.split(' ')[1])  # map(int, input().split())
+            requests_raw = input_test.split('\n')[1:]
+            requests_test = [[] for raw in range(n_requests)]
+            for i, r in enumerate(requests_raw):
+                arrive_time = int(r.split(' ')[0])
+                process_time = int(r.split(' ')[1])
+                requests_test[i].append(arrive_time)
+                requests_test[i].append(process_time)
+            # buffer_size, n_requests = int(line1.split(' ')[0]), int(line1.split(' ')[1]) # map(int, input().split())
+            requests = []
+            for _ in range(n_requests):
+                arrived_at, time_to_process =requests_test[_][0], requests_test[_][1] #map(int, input().split())
+                requests.append(Request(arrived_at, time_to_process))
+
+            buffer = Buffer(buffer_size)
+            responses = process_requests(requests, buffer)
+
+
+            for response in responses:
+                print(response.started_at if response.was_dropped else -1)
 
 
 if __name__ == "__main__":
