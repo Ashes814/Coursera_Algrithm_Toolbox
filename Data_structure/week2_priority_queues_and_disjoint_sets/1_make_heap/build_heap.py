0929d85c8e7f779dab5543ae75f6644831d70e7f
Owen <1534661514@qq.com>
2022-03-30 12:38:51 +0800

    pass week2

--- Data_structure/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
+++ Data_structure/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -1,17 +1,20 @@
 # python3
+# import sys
+# sys.setrecursionlimit(1000000)#表示递归深度为100w
 def Parent(index):
-    return int((index+1)/2 - 1)
+    return int((index + 1) / 2 - 1)
+
 
 def LeftChild(index):
-    return int((index+1)*2 - 1)
+    return int((index + 1) * 2 - 1)
+
 
 def RightChild(index):
     return int((index + 1) * 2)
 
-def SiftDown(index,size, data, swaps):
 
-
-    n = size-1
+def SiftDown(index, size, data, swaps):
+    n = size - 1
     minIndex = index
     left_index = LeftChild(index)
     if left_index <= n and data[left_index] < data[minIndex]:
@@ -22,7 +25,8 @@ def SiftDown(index,size, data, swaps):
     if index != minIndex:
         swaps.append((index, minIndex))
         data[index], data[minIndex] = data[minIndex], data[index]
-        SiftDown(index, n, data, swaps)
+        SiftDown(minIndex, size, data, swaps)
+
 
 def build_heap(data):
     """Build a heap from ``data`` inplace.
@@ -42,34 +46,45 @@ def build_heap(data):
     #         if data[i] > data[j]:
     #             swaps.append((i, j))
     #             data[i], data[j] = data[j], data[i]
-    for i in range(int(size/2) - 2,-1,-1):
-
+    for i in range(int((size - 1 ) / 2) , -1, -1):
         SiftDown(i, size, data, swaps)
     return swaps
 
 
 def main():
-    # n = int(input())
-    # data = list(map(int, input().split()))
-    # assert len(data) == n
-    #
-    # swaps = build_heap(data)
-    #
-    # print(len(swaps))
-    # for i, j in swaps:
-    #     print(i, j)
-    with open('tests/04', 'r') as f:
-        test_data = f.read().strip().split('\n')
-        n = int(test_data[0])
-        data = list(map(int, test_data[1].split(' ')))
-        assert len(data) == n
-        f.close()
+    n = int(input())
+    data = list(map(int, input().split()))
+    assert len(data) == n
+
     swaps = build_heap(data)
 
     print(len(swaps))
-    print(data[0:10])
     for i, j in swaps:
         print(i, j)
+    # with open('tests/04', 'r') as f:
+    #     test_data = f.read().strip().split('\n')
+    #     n = int(test_data[0])
+    #     data = list(map(int, test_data[1].split(' ')))
+    #     assert len(data) == n
+    #     f.close()
+    # with open('tests/04.a', 'r') as f:
+    #     answers = f.read().strip().split('\n')[1:]
+    #     f.close()
+    # data = [10,9,8,7,6,5,4,3,2,1]
+    # n = 10
+
+    # swaps = build_heap(data)
+    #
+    # print(len(swaps))
+    # print(data[0:10])
+    # for i, j,in enumerate(swaps):
+    #     try:
+    #         assert(str(j[0]) + ' ' + str(j[1]) == answers[i])
+    #
+    #     except:
+    #         print(str(j[0]) + ' ' + str(j[1]), end='|')
+    #         print(answers[i])
+
 
 if __name__ == "__main__":
     main()
---
+++ Data_structure/week2_priority_queues_and_disjoint_sets/1_make_heap/test.py
@@ -0,0 +1,8 @@
+n = 10
+
+for i in range(n):
+    print(i)
+
+for i in range(n):
+    print(i)
+    n = 7
