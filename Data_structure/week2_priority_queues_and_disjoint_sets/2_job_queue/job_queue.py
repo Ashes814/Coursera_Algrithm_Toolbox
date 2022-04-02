4992127dc52c396af2be923ad50e2c843faca44e
Owen <1534661514@qq.com>
2022-04-02 20:10:32 +0800

    week2

--- Data_structure/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
+++ Data_structure/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
@@ -7,12 +7,7 @@ AssignedJob = namedtuple("AssignedJob", ["worker", "started_at"])
 
 def assign_jobs(n_workers, jobs):
     # TODO: replace this code with a faster algorithm.
-    result = []
-    next_free_time = [0] * n_workers
-    for job in jobs:
-        next_worker = min(range(n_workers), key=lambda w: next_free_time[w])
-        result.append(AssignedJob(next_worker, next_free_time[next_worker]))
-        next_free_time[next_worker] += job
+
 
     return result
 
