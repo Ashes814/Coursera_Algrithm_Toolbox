cda9da5c170a96a128eaadf60ff778562607f7ab
Owen <1534661514@qq.com>
2022-04-11 11:25:58 +0800

    week3 problem4

--- Data_structure/week3_hash_tables/4_substring_equality/substring_equality.py
+++ Data_structure/week3_hash_tables/4_substring_equality/substring_equality.py
@@ -8,6 +8,12 @@ class Solver:
 	def ask(self, a, b, l):
 		return s[a:a+l] == s[b:b+l]
 
+	def PolyHash(string, prime, multiplier):
+		hash_value = 0
+		for i in range(len(string) - 1, -1, -1):
+			hash_value = (hash_value * multiplier + ord(string[i])) % prime
+		return hash_value
+
 s = sys.stdin.readline()
 q = int(sys.stdin.readline())
 solver = Solver(s)
