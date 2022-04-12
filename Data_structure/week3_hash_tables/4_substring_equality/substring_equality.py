6e338befdad16241507d59e4f560598409f817db
Owen <1534661514@qq.com>
2022-04-12 13:42:20 +0800

    finish course - dATA STRUCTURE

--- Data_structure/week3_hash_tables/4_substring_equality/substring_equality.py
+++ Data_structure/week3_hash_tables/4_substring_equality/substring_equality.py
@@ -1,5 +1,5 @@
 # python3
-
+import time
 import sys
 
 class Solver:
@@ -14,9 +14,16 @@ class Solver:
 			hash_value = (hash_value * multiplier + ord(string[i])) % prime
 		return hash_value
 
-s = sys.stdin.readline()
-q = int(sys.stdin.readline())
+# s = sys.stdin.readline()
+s = 'trololo'
+# q = int(sys.stdin.readline())
+test_list = [(0,0,7), (2,4,3), (3,5,1), (1,3,2)]
 solver = Solver(s)
-for i in range(q):
-	a, b, l = map(int, sys.stdin.readline().split())
+
+t0 = time.time()
+for i in test_list:
+	# a, b, l = map(int, sys.stdin.readline().split())
+	a,b,l = i
+
 	print("Yes" if solver.ask(a, b, l) else "No")
+print("Running Time", time.time()-t0)
--- Data_structure/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
+++ Data_structure/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
@@ -1,23 +1,57 @@
-#!/usr/bin/python3
+# python3
+# Is it a Binary Search Tree? Hard version.
 
 import sys, threading
+sys.setrecursionlimit(10**8) # max depth of recursion
+threading.stack_size(2**27)  # new thread will get stack of such size
 
-sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+class BinarySearchTree:
+    def read(self):
+        self.n = int(sys.stdin.readline())
+        self.key = [0 for i in range(self.n)]
+        self.left = [0 for i in range(self.n)]
+        self.right = [0 for i in range(self.n)]
 
-def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
-  return True
+        for i in range(self.n):
+            [a, b, c] = map(int, sys.stdin.readline().rstrip().split())
+            self.key[i] = a
+            self.left[i] = b
+            self.right[i] = c
+
+    def IsBinarySearchTree(self):
+        if self.n == 0: return True
+        self.result = []
+
+        x = 0
+        def inorder(x):
+            if x != -1:
+                inorder(self.left[x])
+                self.result.append(self.key[x])
+                inorder(self.right[x])
+
+        inorder(x)
+
+        # checks if it is a BST
+        for x in range(1, len(self.result)):
+            if self.result[x] < self.result[x-1]:
+                return False
+
+        # checks whether there are equal elements in the left subtree
+        for x in range(len(self.key)):
+            if self.left[x] != -1:
+                if self.key[x] == self.key[self.left[x]]:
+                    return False
+
+        return True
 
 
 def main():
-  nodes = int(sys.stdin.readline().strip())
-  tree = []
-  for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
-    print("CORRECT")
-  else:
-    print("INCORRECT")
+    nodes = BinarySearchTree()
+    # print(tree)
+    nodes.read()
+    if nodes.IsBinarySearchTree():
+        print("CORRECT")
+    else:
+        print("INCORRECT")
 
 threading.Thread(target=main).start()
